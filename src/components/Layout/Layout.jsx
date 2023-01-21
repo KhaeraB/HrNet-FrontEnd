c450c53e392c756fe14f504b59a0ac3a4b031b56
BELKADI <khaera/belkadi@gmail.com>
2023-01-21 17:20:22 +0100

    logo fetchpriority

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
     <ContentNavBar fluid={true} className="p-0">
       <TitleLogo>
          <Link to="/">
-          <LogoImg src={Logo} alt="Hrnet" href="/" />
+          <LogoImg fetchpriority="high" src={Logo} alt="Hrnet" href="/" />
         <h1>HrNet</h1>
         </Link>
       </TitleLogo>
@@ -31,7 +31,7 @@ export const Footer = () => {
   const content = (
     <ContentFooter fluid={true} className="p-0 d-flex justify-content-center">
       <TitleFooter >
-        <LogoImgFooter src="logo192.png" alt="Hrnet" href="/" />
+        <LogoImgFooter fetchpriority="high" src="logo192.png" alt="Hrnet" href="/" />
         <p>HrNet </p>
         <p>  @2022</p>
       </TitleFooter>
